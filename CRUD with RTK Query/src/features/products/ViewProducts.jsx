6c1afcd5c1e3005e7fdb4bb4d9e200ebc40ceb3d
313shazid313 <hossainshazid567@gmail.com>
2024-10-08 22:28:01 +0600

    RTk query

--- CRUD with RTK Query/src/features/products/ViewProducts.jsx
+++ CRUD with RTK Query/src/features/products/ViewProducts.jsx
@@ -1,22 +1,38 @@
-import { useGetProductsQuery } from "../../services/productsApi"
-
+import { useGetProductsQuery } from "../../services/productsApi";
 
 const ViewProducts = () => {
+  const { data, isError, isLoading } = useGetProductsQuery();
 
-  const {data, isError, isLoading} = useGetProductsQuery();
+  const allData = useGetProductsQuery();
+
+  console.log(allData);
+
   console.log(data);
-console.log(isError)
+
+  console.log(isError);
+
   return (
     <div>
-        
-        <h1>Showing Products</h1>
-        {isLoading && <p>Loading</p>}
-        {isError && <p>Caught error</p>}
-        <section className="product">
-          {}
-        </section>
-    </div>
-  )
-}
+      <h1>Showing Products</h1>
 
-export default ViewProducts
+      {isLoading && <p>Loading</p>}
+
+      {isError && <p>Caught error {isError.message}</p>}
+
+      {!isLoading && !isError && (
+        <section className="product">
+          {data.map((singleData) => {
+            return (
+              <article key={singleData.id}>
+                <h3>{singleData.title}</h3>
+                <p>{singleData.description}</p>
+              </article>
+            );
+          })}
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default ViewProducts;
